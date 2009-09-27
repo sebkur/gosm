6bfc18567b053962717c3ecf08152db888ec8e59
Sebastian Kürten <sebastian@topobyte.de>
2009-09-27 14:01:59 +0000

    added legend for cyclemap

--- gosm.c
+++ gosm.c
@@ -1157,6 +1157,12 @@ static void set_legend(Tileset tileset, int zoom)
 		char * uri_legend = get_abs_uri(path);
 		webkit_web_view_open(WEBKIT_WEB_VIEW(web_legend), uri_legend);
 		free(uri_legend);
+	}else if (tileset == TILESET_CYCLE){
+		char path[100];
+		sprintf(path, GOSM_LEGEND_DIR "cycle/cycle_%d.html", zoom);
+		char * uri_legend = get_abs_uri(path);
+		webkit_web_view_open(WEBKIT_WEB_VIEW(web_legend), uri_legend);
+		free(uri_legend);
 	}else{
 		char * uri_legend = get_abs_uri(GOSM_LEGEND_DIR "no_legend.html");
 		webkit_web_view_open(WEBKIT_WEB_VIEW(web_legend), uri_legend);
